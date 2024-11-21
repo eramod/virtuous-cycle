2093e4b90d4aa25b34493a853a1e25ba581c2d33
eramod <dianne.eramo@gmail.com>
2024-11-20 18:08:19 -0800

    Start auth tests

--- api/tests/test_auth.py
+++ api/tests/test_auth.py
@@ -3,5 +3,16 @@ from flask import g, session # g is the global context object in Flask
 from src.db import get_db
 
 def test_register(client, app):
-  assert client.get('/auth/register').status_code == 200
+  # Expect that a GET request returns a status of undefined
+  assert client.get('/auth/register').status_code == 405
+
+  assert client.post('/auth/register', data={'email':'izzy@paws.com', 'first_name':'Izzy', 'last_name': 'Eramo', 'phone_number': '1111111111', 'password': 'ilovedianne', 'confirm_password': 'ilovedianne'}).status_code == 200
+
+
+def test_register_validation(client, app):
+  assert True == False
+  # TODO: test post errors with parametrize
+
+
+
 
